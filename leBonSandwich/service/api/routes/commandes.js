773d67a3e6d2cf8d07eaece3b0d9b62cb6bb831c
Clément Boulet <clement.boulet60510@gmail.com>
2022-03-08 18:01:02 +0100

    Update d'une ressource uniquement si on a le token

--- leBonSandwich/service/api/routes/commandes.js
+++ leBonSandwich/service/api/routes/commandes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../knex.js');
+const { v4: uuidv4 } = require('uuid');
 
 const methodNotAllowed = require('../errors/methodNotAllowed.js');
 
@@ -40,72 +41,125 @@ router.route('/')
             })
 
     })
+
+router.route('/add')
+    .patch(methodNotAllowed)
+    .delete(methodNotAllowed)
+    .put(methodNotAllowed)
+    .post(function (req, res, next) {
+        //uuidv4
+        const uuid = uuidv4();
+        const created_at = "2019-11-08 13:45:55"
+        const updated_at = created_at;
+        //const created_at = new Date();
+        //const updated_at = created_at;
+        const { livraison, nom, mail, montant } = req.body;
+        const token = uuidv4();
+        const client_id = null;
+        const ref_paiement = null;
+        const date_paiement = null;
+        const mode_paiement = null;
+        const remise = null;
+        const status = 1;
+        knex.from('commande').insert(
+            {
+                'id': uuid,
+                'created_at': created_at,
+                'updated_at': updated_at,
+                'livraison': livraison,
+                'nom': nom,
+                'mail': mail,
+                'montant': montant,
+                'remise': remise,
+                'token': token,
+                'client_id': client_id,
+                'ref_paiement': ref_paiement,
+                'date_paiement': date_paiement,
+                'mode_paiement': mode_paiement,
+                'status': status
+            }
+        ).then(() => {
+            res.status(201).json({
+                "token" : token,
+                "Location" : `/commandes/${uuid}`
+            })
+        }
+        ).catch((err) => {
+            res.status(500).json({
+                "type": "error",
+                "error": 500,
+                "message": `Erreur de connexion à la base de données ` + err
+            });
+        })
+    })
+    .get(methodNotAllowed)
+
 router.route('/:id')
     .patch(methodNotAllowed)
     .delete(methodNotAllowed)
     .post(methodNotAllowed)
     .get(function (req, res, next) {
-        if (req.query.embed && req.query.embed === "items" ) {
+        if (req.query.embed && req.query.embed === "items") {
             knex.from('commande')
-            .select('id', 'mail', 'nom', 'created_at', 'livraison', 'montant')
-            .where({
-                'id': req.params.id
-            }).first()
-            .then((commande) => {
-                console.log(commande)
-                if (commande === null) {
-                    res.status(404).json({
-                        "type": "error",
-                        "error": 404,
-                        "message": `ressource non disponible : ${req.params.id}`
-                    });
-                } else {
-                    let order = {
-                        type: "ressource",
-                        commande: commande,
+                .select('id', 'mail', 'nom', 'created_at', 'livraison', 'montant')
+                .where({
+                    'id': req.params.id
+                }).first()
+                .then((commande) => {
+                    console.log(commande)
+                    if (commande === null) {
+                        res.status(404).json({
+                            "type": "error",
+                            "error": 404,
+                            "message": `ressource non disponible : ${req.params.id}`
+                        });
+                    } else {
+                        let order = {
+                            type: "ressource",
+                            commande: commande,
+                        }
+                        knex.from('item')
+                            .select(' id', 'libelle', 'tarif', 'quantite ')
+                            .where({
+                                'command_id': req.params.id
+                            })
+                            .then((items) => {
+                                if (items === null) {
+                                    order.links =
+                                    {
+                                        items: {
+                                            href: `/commandes/${req.params.id}/items`
+                                        },
+                                        self: {
+                                            href: `/commandes/${req.params.id}/`
+                                        }
+                                    }
+                                    res.status(200).json(order)
+                                } else {
+                                    order.commande.items = Array()
+                                    items.forEach(item => {
+                                        order.commande.items.push(item)
+                                    });
+                                    order.links =
+                                    {
+                                        items: {
+                                            href: `/commandes/${req.params.id}/items`
+                                        },
+                                        self: {
+                                            href: `/commandes/${req.params.id}/`
+                                        }
+                                    }
+                                    res.status(200).json(order)
+                                }
+                            })
                     }
-                    knex.from('item')
-                        .select(' id', 'libelle', 'tarif', 'quantite ')
-                        .where({
-                            'command_id': req.params.id
-                        })
-                        .then((items) => {
-                            if (items === null) {
-                                order.links =
-                                {
-                                    items: {
-                                        href: `/commandes/${req.params.id}/items`
-                                    },
-                                    self: {
-                                        href: `/commandes/${req.params.id}/`
-                                    }
-                                }
-                                res.status(200).json(order)
-                            } else {
-                                order.commande.items = Array()
-                                items.forEach(item => {
-                                    order.commande.items.push(item)
-                                });
-                                order.links =
-                                {
-                                    items: {
-                                        href: `/commandes/${req.params.id}/items`
-                                    },
-                                    self: {
-                                        href: `/commandes/${req.params.id}/`
-                                    }
-                                }
-                                res.status(200).json(order)
-                            }
-                        })
-                }
-            }).catch((err) => {
-                res.status(500).json({
-                    "type": "error",
-                    "error": 500,
-                    "message": `Erreur de connexion à la base de données` + err
-                });
-            })
+                }).catch((err) => {
+                    res.status(500).json({
+                        "type": "error",
+                        "error": 500,
+                        "message": `Erreur de connexion à la base de données` + err
+                    });
+                })
         } else {
             knex.from('commande')
                 .select('*')
@@ -146,42 +200,67 @@ router.route('/:id')
         }
     })
     .put(async function (req, res, next) {
-        let commande;
-        console.log(htmlEntities(req.body.nom))
-        try {
-            if (req.body.nom) {
-                req.body.nom = htmlEntities(req.body.nom)
-            }
-            if (req.body.mail) {
-                req.body.mail = htmlEntities(req.body.mail)
-            }
-            if (req.body.livraison) {
-                req.body.livraison = htmlEntities(req.body.livraison)
-            }
-            await knex.from('commande').where('id', req.params.id).update({
-                nom: req.body.nom,
-                mail: req.body.mail,
-                livraison: req.body.livraison,
-                updated_at: new Date()
-            }).then((rows) => {
-                if (rows === 0) {
+        let token = req.body.token
+        knex.from('commande')
+            .select('id','token')
+            .where({
+                'id': req.params.id,
+                'token': token
+                })
+            .first()
+            .then((commande) => {
+                console.log(commande)
+                if (! (typeof commande === "undefined")){
+                    try {
+                        if (req.body.nom) {
+                            req.body.nom = htmlEntities(req.body.nom)
+                        }
+                        if (req.body.mail) {
+                            req.body.mail = htmlEntities(req.body.mail)
+                        }
+                        if (req.body.livraison) {
+                            req.body.livraison = htmlEntities(req.body.livraison)
+                        }
+                        knex.from('commande').where('id', req.params.id).update({
+                            nom: req.body.nom,
+                            mail: req.body.mail,
+                            livraison: req.body.livraison,
+                            updated_at: new Date()
+                        }).then((rows) => {
+                            if (rows === 0) {
+                                res.status(404).json({
+                                    "type": "error",
+                                    "error": 404,
+                                    "message": `commande ${req.params.id} non trouvée`
+                                });
+                            } else {
+                                res.status(201).json(commande);
+                            }
+                        });
+                    } catch (error) {
+                        console.log(error);
+                        res.status(500).json({
+                            "type": "error",
+                            "error": 500,
+                            "message": `Impossible d'executer la requete`
+                        });
+                    }
+                }else{
                     res.status(404).json({
                         "type": "error",
                         "error": 404,
-                        "message": `commande ${req.params.id} non trouvée`
+                        "message": `Impossible d'executer la requete, le token n'a pas l'air bon`
                     });
-                } else {
-                    res.status(204).json(commande);
                 }
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({
-                "type": "error",
-                "error": 500,
-                "message": `Impossible d'executer la requete`
-            });
-        }
+            
+            }).catch((err) => {
+                console.log(err)
+                res.status(500).json({
+                    "type": "error",
+                    "error": 500,
+                    "message": `Erreur de connexion à la base de données`
+                });
+            })
         function htmlEntities(str) {
             if (str === "") {
                 return null
@@ -222,72 +301,5 @@ router.route('/:id/items')
                 }
             })
     });
-/*
-router.route('/:id/items')
-    .patch(methodNotAllowed)
-    .delete(methodNotAllowed)
-    .post(methodNotAllowed)
-    .get(function (req, res, next) {
-        knex.from('commande')
-            .select('id', 'mail', 'nom', 'created_at', 'livraison', 'montant')
-            .where({
-                'id': req.params.id
-            }).first()
-            .then((commande) => {
-                console.log(commande)
-                if (commande === null) {
-                    res.status(404).json({
-                        "type": "error",
-                        "error": 404,
-                        "message": `ressource non disponible : ${req.params.id}`
-                    });
-                } else {
-                    let order = {
-                        type: "ressource",
-                        commande: commande,
-                    }
-                    knex.from('item')
-                        .select(' id', 'libelle', 'tarif', 'quantite ')
-                        .where({
-                            'command_id': req.params.id
-                        })
-                        .then((items) => {
-                            if (items === null) {
-                                order.links =
-                                {
-                                    items: {
-                                        href: `/commandes/${req.params.id}/items`
-                                    },
-                                    self: {
-                                        href: `/commandes/${req.params.id}/`
-                                    }
-                                }
-                                res.status(200).json(order)
-                            } else {
-                                order.commande.items = Array()
-                                items.forEach(item => {
-                                    order.commande.items.push(item)
-                                });
-                                order.links =
-                                {
-                                    items: {
-                                        href: `/commandes/${req.params.id}/items`
-                                    },
-                                    self: {
-                                        href: `/commandes/${req.params.id}/`
-                                    }
-                                }
-                                res.status(200).json(order)
-                            }
-                        })
-                }
-            }).catch((err) => {
-                res.status(500).json({
-                    "type": "error",
-                    "error": 500,
-                    "message": `Erreur de connexion à la base de données` + err
-                });
-            })
-    });
- */
+
 module.exports = router;
